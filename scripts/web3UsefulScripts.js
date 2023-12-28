0a9cd9f0a3c35baebe96eade0cb2451f58dc5c84
madjarevicn <nikola.madjarevic@gmail.com>
2023-12-28 23:34:19 +0100

    Add annotations.

--- scripts/web3UsefulScripts.js
+++ scripts/web3UsefulScripts.js
@@ -1,4 +1,9 @@
-
+/**
+ *
+ * @param ethers instance of ethers
+ * @param methodNameAndArgs should be an example "transfer(address,uint256)"
+ * @returns {string}
+ */
 const getMethodId = (ethers, methodNameAndArgs) => {
     return ethers.utils.id(methodNameAndArgs).substring(0, 10);
 }
