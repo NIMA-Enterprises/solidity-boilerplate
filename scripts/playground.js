931c5d379d4f50d05dae7cec096e80d51a87519b
madjarevicn <nikola.madjarevic@gmail.com>
2023-12-29 10:01:39 +0100

    Add some more useful scripts & playground.

---
+++ scripts/playground.js
@@ -0,0 +1,15 @@
+const hre = require('hardhat');
+const {getBlock} = require("./web3UsefulScripts");
+const { ethers } = hre;
+
+async function main() {
+    const block = await getBlock(ethers);
+    console.log(block)
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
--- scripts/web3UsefulScripts.js
+++ scripts/web3UsefulScripts.js
@@ -8,6 +8,33 @@ const getMethodId = (ethers, methodNameAndArgs) => {
   return ethers.utils.id(methodNameAndArgs).substring(0, 10);
 };
 
+/**
+ * Default value would be the latest block
+ * @param ethers
+ * @param blockNumber number of the requested block
+ * @returns {*}
+ */
+const getBlock = async (ethers, blockNumber) => {
+    return await ethers.provider.getBlock(blockNumber || 'latest');
+}
+
+
+/**
+ * Get transactions in block, by default will return from the last one.
+ * @param ethers
+ * @param blockNumber
+ * @returns {Promise<*>}
+ */
+const getTransactionsInBlock = async (ethers, blockNumber) => {
+    const block = getBlock(ethers,blockNumber);
+    return block.transactions;
+}
+
+
+
+
 module.exports = {
-  getMethodId,
+    getMethodId,
+    getBlock,
+    getTransactionsInBlock
 };
